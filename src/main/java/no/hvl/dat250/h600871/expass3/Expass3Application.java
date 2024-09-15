66d3dba6669fd81fd3f1ad43ebf34f47f8d446f9
Lars Magnus Nordeide <600871@stud.hvl.no>
2024-09-15 05:19:37 +0200

    Init Spring, Vite, Svelte

---
+++ src/main/java/no/hvl/dat250/h600871/expass3/Expass3Application.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.h600871.expass3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Expass3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Expass3Application.class, args);
+    }
+
+}
---
+++ src/test/java/no/hvl/dat250/h600871/expass3/Expass3ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat250.h600871.expass3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expass3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
